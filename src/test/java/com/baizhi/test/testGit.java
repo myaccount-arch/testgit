b5047a7440419961a32b7079fb0da4701993e49d
gaof <gaof18734741934@163.com>
2019-12-11 21:34:28 +0800

    first commit

--- src/test/java/com/baizhi/test/testGit.java
+++ src/test/java/com/baizhi/test/testGit.java
@@ -3,5 +3,6 @@ package com.baizhi.test;
 public class testGit {
     public static void main(String[] args) {
         System.out.println("这是我的git测试");
+        System.out.println("这是我第二次测试");
     }
 }
