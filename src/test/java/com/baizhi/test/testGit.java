3095aa6b0591fcfa85a66ceaa74ffc31d3b0bfce
myaccount-arch <58757713+myaccount-arch@users.noreply.github.com>
2019-12-11 21:40:06 +0800

    Update testGit.java

--- src/test/java/com/baizhi/test/testGit.java
+++ src/test/java/com/baizhi/test/testGit.java
@@ -4,5 +4,7 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("这是我的git测试");
         System.out.println("这是我第二次测试");
+           System.out.println("这是我第三次测试");
+        System.out.println("这是我第四次测试");
     }
 }
